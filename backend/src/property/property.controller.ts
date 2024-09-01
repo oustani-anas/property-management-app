1b385838608d1d574d286b9ab50e422a59a989ff
oustani-anas <anas.migos20@gmail.com>
2024-09-01 01:10:24 +0100

    building authentication

--- backend/src/property/property.controller.ts
+++ backend/src/property/property.controller.ts
@@ -2,6 +2,8 @@
 import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { PropertyService } from './property.service';
 import { Property, Prisma } from '@prisma/client';
+import { CreateTenantDto } from 'src/tenant/dto/CreateTenantDto';
+import { UpdateTenantDto } from 'src/tenant/dto/UpdateTenantDto';
 
 @Controller('properties')
 export class PropertyController {
@@ -35,4 +37,30 @@ export class PropertyController {
   async remove(@Param('id') id: string): Promise<Property> {
     return this.propertyService.remove(+id);
   }
+
+  @Post(':propertyId/tenants')
+  async addTenant(
+    @Param('propertyId') propertyId: number,
+    @Body() tenantData: CreateTenantDto,
+  ) {
+  return this.propertyService.addTenant(propertyId, tenantData);
+  }
+
+  @Delete(':propertyId/tenants/:tenantId')
+  async removeTenant(
+    @Param('propertyId') propertyId: number,
+    @Param('tenantId') tenantId: number,
+  ) {
+  return this.propertyService.removeTenant(propertyId, tenantId);
+  }
+
+  @Put(':propertyId/tenants/:tenantId')
+  async updateTenant(
+    @Param('propertyId') propertyId: number,
+    @Param('tenantId') tenantId: number,
+    @Body() tenantData: UpdateTenantDto,
+  ) {
+  return this.propertyService.updateTenant(propertyId, tenantId, tenantData);
+  }
+
 }
--- backend/src/property/property.service.ts
+++ backend/src/property/property.service.ts
@@ -2,6 +2,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { Property, Prisma } from '@prisma/client';
+import { CreateTenantDto } from 'src/tenant/dto/CreateTenantDto';
+import { UpdateTenantDto } from 'src/tenant/dto/UpdateTenantDto';
 
 @Injectable()
 export class PropertyService {
@@ -26,5 +28,51 @@ export class PropertyService {
   async remove(id: number): Promise<Property> {
     return this.prisma.property.delete({ where: { id } });
   }
+
+  //tenet management
+  async addTenant(propertyId: number, tenantData: CreateTenantDto): Promise<Property> {
+    return this.prisma.property.update({
+      where: { id: propertyId },
+      data: {
+        tenants: {
+          create: tenantData,
+        },
+      },
+      include: {
+        tenants: true, // Include tenants in the response
+      },
+    });
+  }
+  
+  async removeTenant(propertyId: number, tenantId: number): Promise<Property> {
+    return this.prisma.property.update({
+      where: { id: propertyId },
+      data: {
+        tenants: {
+          delete: { id: tenantId },
+        },
+      },
+      include: {
+        tenants: true, // Include tenants in the response
+      },
+    });
+  }
+  async updateTenant(propertyId: number, tenantId: number, tenantData: UpdateTenantDto): Promise<Property> {
+    return this.prisma.property.update({
+      where: { id: propertyId },
+      data: {
+        tenants: {
+          update: {
+            where: { id: tenantId },
+            data: tenantData,
+          },
+        },
+      },
+      include: {
+        tenants: true, // Include tenants in the response
+      },
+    });
+  }
+    
 }
 
